465e06bca4174004054d7cc8755aa428e4fc8d50
jaber1212 <jjjjjgj5@gmail.com>
2025-10-25 22:20:21 -0700

    check

--- mainapp/serializers.py
+++ mainapp/serializers.py
@@ -120,7 +120,7 @@ class ProfileSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ["first_name", "phone", "is_verified", "op_code","email"]  # first_name acts as name
+        fields = ["name", "phone", "is_verified", "op_code","email"]  # first_name acts as name
 
 class ProfileUpdateSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=120)
